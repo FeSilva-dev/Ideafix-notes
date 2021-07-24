81957f990a73a4b21b1a5eb6c6a385b106d5119e
FeSilva-dev <feliper.silva011@gmail.com>
2021-07-24 16:39:44 -0300

    create express base notes app

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const path = require('path');
+
+const router = require('./routes');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(router);
+
+const port = 3333;
+
+app.listen(port, () => {
+  console.log(`Apllication is running on port ${port}`)
+});
